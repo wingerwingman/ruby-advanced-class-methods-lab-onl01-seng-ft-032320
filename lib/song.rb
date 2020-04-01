2640afe9f3eefe54bd2b56aa349e2862072cb477
Justin Winger <justinwinger4@gmail.com>
2020-04-01 12:48:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,46 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
-
+    
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new 
+    song.save
+    song
+  end
+  
+  def self.new_by_name(string_name)
+    song = self.new 
+    song.name = string_name
+    song
+  end 
+  
+  def self.create_by_name(string_name)
+    song = self.new 
+    song.name = string_name
+    song.save
+    song
+  end 
+  
+  def self.find_by_name(name)
+    self.all.find { |song| song.name == name }
+  end
+  
+  def find_or_create_by_name(name)
+    result = self.find_by_name(name)
+    if result
+      result
+    else
+      self.create_by_name(name)
+    end
+  end
+
 
 end
