9c2f49519ace222f1f63a7e3083b3419b9d140c1
Justin Winger <justinwinger4@gmail.com>
2020-04-01 13:14:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,11 +35,11 @@ class Song
   end
   
   def find_or_create_by_name(name)
-    #if self.find_by_name(name) == nil 
-        #self.create_by_name(name)
-      #else
-        #self.find_by_name(name)
-      #end
+    if self.find_by_name(name) == nil 
+        self.create_by_name(name)
+      else
+        self.find_by_name(name)
+      end
     # self.find_by_name(name) || self.create_by_name(name)
   end
   
