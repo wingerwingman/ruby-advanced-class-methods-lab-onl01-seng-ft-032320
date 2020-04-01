8eb4410779da6f1c5e47d7c500a2dbc3ba0f2d03
Justin Winger <justinwinger4@gmail.com>
2020-04-01 13:31:03 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,78 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
-
+    
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new 
+    song.save
+    song
+  end
+  
+  def self.new_by_name(string_name)
+    song = self.new 
+    song.name = string_name
+    song
+  end 
+  
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    song = self.all.select { |song| song.name == name }
+    song[0]
+  end
+
+  def self.find_or_create_by_name(name)
+    if !find_by_name(name)
+      create_by_name(name)
+    else
+      find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name[0]}
+  end
+  
+  def self.alphabetical
+    self.all.sort_by { |song| song.name[0]}
+  end
+
+  def self.new_from_filename(filename)
+    song_data = filename.sub!(/.mp3/,'').split(' - ')
+
+    song = self.new
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
+
+    song
+  end
+
+  def self.create_from_filename(filename)
+    # binding.pry
+    song_data = filename.sub!(/.mp3/,'').split(' - ')
+
+    song = self.create
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
+
+    song
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
+
 
 end
